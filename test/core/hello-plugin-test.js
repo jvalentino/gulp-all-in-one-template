410e1ee507845d80487481e7ff6a39ea5a7be21a
jvalentino <jvalentino2@gmail.com>
2022-06-28 19:40:23 -0500

    switched over to VSC

---
+++ test/core/hello-plugin-test.js
@@ -0,0 +1,17 @@
+
+var expect    = require("chai").expect;
+const subject = require("../../src/core/hello-plugin");
+
+describe("hello-plugin.js", function() {
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+      console.log('hi')
+    });
+  });
+
+  describe("Hex to RGB conversion", function() {
+    it("converts the basic colors", function() {
+
+    });
+  });
+});
